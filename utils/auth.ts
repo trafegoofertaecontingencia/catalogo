0eb06753c5657ac02ba756e575c55b36606dfbce
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-06-23 19:31:50 -0300

    ajuste nas variaveis de ambiente

--- utils/auth.ts
+++ utils/auth.ts
@@ -25,8 +25,9 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       // clientId: process.env.GOOGLE_ID as string,
       // clientSecret: process.env.GOOGLE_SECRET as string,
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+
     }),
   ],
   callbacks: {
