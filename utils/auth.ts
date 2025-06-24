dd8df4a30629188e14d11efe276d1f5865033b04
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-06-24 11:03:14 -0300

    log adicionado

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,14 +1,13 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { Adapter } from "next-auth/adapters"
 import { prisma } from "@/utils/connect"
+
+console.log("GOOGLE_ID NO BUILD:", process.env.AUTH_GOOGLE_ID)
+
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: <Adapter>PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   secret: process.env.AUTH_SECRET,
 })
-
-export const { GET, POST } = handlers
-export const dynamic = "force-dynamic"
