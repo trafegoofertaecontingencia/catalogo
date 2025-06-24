d39225a19b13188a4031ccf2899461b30dc63789
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-06-24 10:30:44 -0300

    alterações no auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -6,4 +6,8 @@ import { prisma } from "@/utils/connect"
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  secret: process.env.AUTH_SECRET,
 })
+
+export const { GET, POST } = handlers
+export const dynamic = "force-dynamic"
