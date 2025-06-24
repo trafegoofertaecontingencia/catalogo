f29536a5f5a26d9b7df3da502cd1955ca6d90301
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-06-24 11:18:49 -0300

    alterações no auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-console.log("GOOGLE_ID NO BUILD:", process.env.AUTH_GOOGLE_ID)
-
- 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: await (async () => {
+    const { PrismaAdapter } = await import("@auth/prisma-adapter")
+    const { prisma } = await import("@/utils/connect")
+    return PrismaAdapter(prisma)
+  })(),
   providers: [Google],
   secret: process.env.AUTH_SECRET,
 })
+
+export const { GET, POST } = handlers
+export const dynamic = "force-dynamic"
