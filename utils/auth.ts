10da61b706a176d639ca4a1620de9a04dc3a4d45
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-06-24 11:13:34 -0300

    remoção do adapter

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,13 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/utils/connect"
 
 console.log("GOOGLE_ID NO BUILD:", process.env.AUTH_GOOGLE_ID)
 
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [Google],
   secret: process.env.AUTH_SECRET,
 })
